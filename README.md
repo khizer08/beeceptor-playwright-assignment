0a0ad426b3c1a3209267d679c53749ce6682e583
khizer08 <syedkhizer2004@gmail.com>
2026-07-14 14:41:09 +0530

    Implemented Beeceptor HTTP Callout automation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Beeceptor HTTP Callout Automation
+
+## Overview
+This project automates Beeceptor's HTTP Callout feature using Playwright.
+
+## Features
+- Login using OTP
+- Reuse Beeceptor endpoint
+- Create HTTP Callout Rule
+- Trigger API request
+- Verify response
+- Automatically clean up test data by deleting the created rule
+
+## Tech Stack
+- Playwright
+- JavaScript
+- Node.js
+
+## Run
+
+npm install
+npx playwright test tests/beeceptor.spec.js --headed --project=chromium
---
+++ tests/beeceptor.spec.js
@@ -0,0 +1,104 @@
+import { test, expect } from "@playwright/test";
+
+test("Beeceptor HTTP Callout Assignment", async ({ page }) => {
+  await page.goto("https://app.beeceptor.com/login");
+
+  await page.getByRole("button", { name: "Sign in with OTP" }).click();
+  await page.waitForTimeout(800);
+
+  await page.getByRole("textbox", { name: "Email address" }).click();
+  await page
+    .getByRole("textbox", { name: "Email address" })
+    .fill("syedkhizer1308@gmail.com");
+
+  await page.waitForTimeout(2000);
+
+  await page.getByRole("button", { name: "Send OTP" }).click();
+
+  await page.pause();
+
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+  await page.waitForTimeout(5000);
+
+  await page.getByRole("button", { name: "user-avatar Syed Kh..." }).click();
+  await page.waitForTimeout(3000);
+
+  await page.getByRole("link", { name: "Your Endpoints" }).click();
+  await page.waitForTimeout(4000);
+
+  await page
+    .getByRole("link", { name: "#khizer-playwright-beeceptor" })
+    .click();
+  await page.waitForTimeout(3000);
+
+  await page.locator("a").filter({ hasText: "Mock Rules (2)" }).click();
+  await page.waitForTimeout(5000);
+
+  await page.getByRole("button", { name: "Toggle Dropdown" }).click();
+  await page.waitForTimeout(2000);
+
+  await page.getByRole("link", { name: " New Callout Rule" }).click();
+  await page.waitForTimeout(2000);
+  
+  await page.getByRole("combobox").first().selectOption("POST");
+  
+  await page.getByRole("textbox", { name: "e.g. /api/path" }).click();
+  await page.getByRole("textbox", { name: "e.g. /api/path" }).fill("/test");
+  await page.waitForTimeout(3000);
+  
+  await page
+  .locator("#v2CollapseOne")
+  .getByRole("combobox")
+  .selectOption("async");
+  await page.waitForTimeout(3000);
+  
+  await page.getByRole("combobox").nth(3).selectOption("POST");
+  
+  await page
+  .getByRole("textbox", { name: "https://your-webhook-endpoint" })
+  .click();
+  await page.waitForTimeout(3000);
+  
+  await page
+  .getByRole("textbox", { name: "https://your-webhook-endpoint" })
+  .fill("https://webhook.site/ad167e41-c142-497c-ab5e-4c1e3b19443a");
+  await page.waitForTimeout(3000);
+
+
+  await page.getByRole("button", { name: " Save" }).click();
+  await page.waitForTimeout(3000);
+
+  await page.getByRole("button", { name: "Close" }).click();
+  await page.waitForTimeout(10000);
+  
+  const response = await page.request.post(
+    "https://khizer-playwright-beeceptor.free.beeceptor.com/test",
+    {
+      data: {
+        name: "Syed Khizer",
+        role: "Software Developer Intern",
+        source: "Playwright Assignment",
+      },
+    },
+  );
+  
+  expect(response.status()).toBe(200);
+  await page.waitForTimeout(8000);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.message).toBe("This is an instant response!");
+
+  await page.waitForTimeout(3000);
+
+  await page.locator("a").filter({ hasText: "Mock Rules (3)" }).click();
+  await page.waitForTimeout(500);
+
+  page.once("dialog", async (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    await dialog.accept();
+  });
+
+  await page.getByRole("button", { name: "Delete rule" }).click();
+  await page.getByRole("button", { name: "Close" }).click();
+});
